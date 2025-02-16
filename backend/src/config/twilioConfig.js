9e6794ea8101708212808bcb05131cd272fa4caf
Sameer Choudhary <sameer.sc62@gmail.com>
2025-02-16 13:10:03 +0530

    Moved .gitignore to root

--- backend/src/config/twilioConfig.js
+++ backend/src/config/twilioConfig.js
@@ -2,9 +2,9 @@ const twilio = require("twilio");
 require("dotenv").config()
 
 // Retrieve environment variables
-const accountSid = process.env.ACCOUNT_SID || "ACbc5c7042247e73d1e2078970ee73ce3f"
-const authToken = process.env.AUTH_TOKEN || "b1fe2e20062dafcd7e1b27d39971d862"
-const verifyServiceSid = process.env.VERIFY_SERVICE_SID || "VAb718f0f62d94141631f70d3890626938"
+const accountSid = process.env.ACCOUNT_SID 
+const authToken = process.env.AUTH_TOKEN 
+const verifyServiceSid = process.env.VERIFY_SERVICE_SID 
 
 // Check for missing values
 if (!accountSid || !authToken || !verifyServiceSid) {
