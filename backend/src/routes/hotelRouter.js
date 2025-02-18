b71731a626e4ad71f7123d4378cef8b4226aa8db
Sameer Choudhary <sameer.sc62@gmail.com>
2025-02-18 17:59:31 +0530

    Updated frontend and hotel API, added build files

--- backend/src/routes/hotelRouter.js
+++ backend/src/routes/hotelRouter.js
@@ -6,26 +6,26 @@ const hotelRoute = express.Router();
 
 
 // Create a new hotel with image uploads
-hotelRoute.post('/create', upload.array('images', 5), createHotel);  // http://localhost:8000/api/v1/hotels/create
+hotelRoute.post('/create', upload.array('images', 5), createHotel);  // ${API_URL}/api/v1/hotels/create
 
 // Endpoint to save location data
-// hotelRoute.post('/get-location', getLocation);  // http://localhost:8000/api/v1/hotels/get-location
+// hotelRoute.post('/get-location', getLocation);  // ${API_URL}/api/v1/hotels/get-location
 
 // Retrieve all hotels
-hotelRoute.get('/hotels', getAllHotels);  // http://localhost:8000/api/v1/hotels/hotels
+hotelRoute.get('/hotels', getAllHotels);  // ${API_URL}/api/v1/hotels/hotels
 
 // Find nearest hotels based on location
-hotelRoute.get('/filter', filterHotels);  // http://localhost:8000/api/v1/hotels/filter
+hotelRoute.get('/filter', filterHotels);  // ${API_URL}/api/v1/hotels/filter
 
 // Retrieve one hotel by phone
-hotelRoute.get('/:phone', getOneHotel);  // http://localhost:8000/api/v1/hotels/:phone
+hotelRoute.get('/:phone', getOneHotel);  // ${API_URL}/api/v1/hotels/:phone
 
 
-hotelRoute.get('/:id', getOneHotel);  // http://localhost:8000/api/v1/hotels/:phone
+hotelRoute.get('/:id', getOneHotel);  // ${API_URL}/api/v1/hotels/:phone
 
 
 // Find nearest hotels based on location
-hotelRoute.post('/find-nearest', findNearestHotels);  // http://localhost:8000/api/v1/hotels/find-nearest
+hotelRoute.post('/find-nearest', findNearestHotels);  // ${API_URL}/api/v1/hotels/find-nearest
 
 
 module.exports = hotelRoute;
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -4,13 +4,13 @@ const { sendOtp, verifyOtp, completeRegistration } = require("../controllers/use
 const userRoute = express.Router();
 
 userRoute.post("/send-otp", sendOtp)
-// http://localhost:8000/api/v1/user/send-otp
+// ${API_URL}/api/v1/user/send-otp
 
 userRoute.post("/verify", verifyOtp )
-// http://localhost:8000/api/v1/user/verify-otp
+// ${API_URL}/api/v1/user/verify-otp
 
 userRoute.post("/register", completeRegistration)
-// http://localhost:8000/api/v1/user/register
+// ${API_URL}/api/v1/user/register
 
 
 
