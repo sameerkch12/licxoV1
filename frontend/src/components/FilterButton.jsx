bc91db68357f976866b1e9b47e453d33336abd1c
Abdulrazzaque6 <94055116+Abdulrazzaque6@users.noreply.github.com>
2025-03-15 14:11:23 +0530

    Update by Abdul

--- frontend/src/components/FilterButton.jsx
+++ frontend/src/components/FilterButton.jsx
@@ -5,14 +5,10 @@ export const FilterButton = ({ onClick, hasActiveFilters }) => {
   return (
     <button
       onClick={onClick}
-      className={`flex items-center gap-2 px-4 py-2 rounded-md shadow-md transition duration-300 ml-auto ${
-        hasActiveFilters
-          ? "bg-gradient-to-r from-gray-800 to-gray-900 text-white hover:from-gray-900 hover:to-gray-800"
-          : "bg-gradient-to-r from-gray-800 to-gray-900 text-white hover:from-gray-900 hover:to-gray-800"
-      }`}
+      className="flex items-center gap-2 px-2 py-2 rounded-md shadow-md transition duration-300 ml-auto"
     >
-      <SlidersHorizontal size={18} className="mr-1" />
-      <span className="font-medium text-sm">Filters</span>
+      <SlidersHorizontal size={30} className="text-dark-gray" /> {/* Increase size and apply dark gray color */}
+      
       {hasActiveFilters && (
         <span className="w-2 h-2 rounded-full bg-white ml-1"></span>
       )}
